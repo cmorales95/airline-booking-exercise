d967fea06ff0d535607172d0b1c12e67fb8fcc88
Cristian Morales <crisdavidmm95.dev@gmail.com>
2022-11-24 20:37:24 -0500

    clean main environment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,5 @@ import presentation.flight.FlightHTMLFormat
 import presentation.ticket.TicketConsoleFormat
 
 fun main(args: Array<String>) {
-    val getFlights = GetFlights(FlightHTMLFormat())
-    val flightFormat = getFlights.invoke()
-    println(flightFormat)
 
-    val getTickets = GetTickets(TicketConsoleFormat())
-    val ticketFormatted = getTickets.invoke()
-    println(ticketFormatted)
 }
